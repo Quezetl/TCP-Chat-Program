26adc55dbbe0346ed37d6f118adc29be1fc3a69b
OscarDU123 <91642585+OscarDU123@users.noreply.github.com>
2022-02-28 13:34:48 -0800

    Update TCPListener.h

--- TCPListener.h
+++ TCPListener.h
@@ -1,171 +1,49 @@
-#include "TcpListener.h"
 
+#pragma once
 
-int TCPListener::init()
-{
-	// Initialze winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
 
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0)
-	{
-		return wsOk;
-	}
+#pragma comment (lib, "ws2_32.lib")
 
-	// Create a socket
-	m_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (m_socket == INVALID_SOCKET)
-	{
-		return WSAGetLastError();
-	}
-
-	// Bind the ip address and port to a socket
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(m_port);
-	inet_pton(AF_INET, m_ipAddress, &hint.sin_addr);
-
-	if (bind(m_socket, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR)
-	{
-		return WSAGetLastError();
-	}
-
-	// Tell Winsock the socket is for listening 
-	if (listen(m_socket, SOMAXCONN) == SOCKET_ERROR)
-	{
-		return WSAGetLastError();
-	}
-
-	// Create the master file descriptor set and zero it
-	FD_ZERO(&m_master);
-
-	// Add our first socket that we're interested in interacting with; the listening socket!
-	// It's important that this socket is added for our server or else we won't 'hear' incoming
-	// connections 
-	FD_SET(m_socket, &m_master);
-
-	return 0;
-}
-
-int TCPListener::run()
-{
-	// this will be changed by the \quit command (see below, bonus not in video!)
-	bool running = true;
-
-	while (running)
-	{
-		// Make a copy of the master file descriptor set, this is SUPER important because
-		// the call to select() is _DESTRUCTIVE_. The copy only contains the sockets that
-		// are accepting inbound connection requests OR messages. 
-
-		// E.g. You have a server and it's master file descriptor set contains 5 items;
-		// the listening socket and four clients. When you pass this set into select(), 
-		// only the sockets that are interacting with the server are returned. Let's say
-		// only one client is sending a message at that time. The contents of 'copy' will
-		// be one socket. You will have LOST all the other sockets.
-
-		// SO MAKE A COPY OF THE MASTER LIST TO PASS INTO select() !!!
-
-		fd_set copy = m_master;
-
-		// See who's talking to us
-		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
-
-		// Loop through all the current connections / potential connect
-		for (int i = 0; i < socketCount; i++)
-		{
-			// Makes things easy for us doing this assignment
-			SOCKET sock = copy.fd_array[i];
-
-			// Is it an inbound communication?
-			if (sock == m_socket)
-			{
-				// Accept a new connection
-				SOCKET client = accept(m_socket, nullptr, nullptr);
-
-				// Add the new connection to the list of connected clients
-				FD_SET(client, &m_master);
-
-				onClientConnected(client);
-			}
-			else // It's an inbound message
-			{
-				char buf[4096];
-				ZeroMemory(buf, 4096);
-
-				// Receive message
-				int bytesIn = recv(sock, buf, 4096, 0);
-				if (bytesIn <= 0)
-				{
-					// Drop the client
-					onClientDisconnected(sock);
-					closesocket(sock);
-					FD_CLR(sock, &m_master);
-				}
-				else
-				{
-					onMessageReceived(sock, buf, bytesIn, 1);
-				}
-			}
-		}
-	}
-	//termination
-	// Remove the listening socket from the master file descriptor set and close it
-	// to prevent anyone else trying to connect.
-	FD_CLR(m_socket, &m_master);
-	closesocket(m_socket);
-
-	while (m_master.fd_count > 0)
-	{
-		// Get the socket number
-		SOCKET sock = m_master.fd_array[0];
-
-		// Remove it from the master file list and close the socket
-		FD_CLR(sock, &m_master);
-		closesocket(sock);
-	}
-
-	// Cleanup winsock
-	WSACleanup();
-	return 0;
-}
-
-
-void TCPListener::sendToClient(int clientSocket, const char* msg, int length)//client first connects to the server
-{
-	send(clientSocket, msg, length, 0);
-}
-
-void TCPListener::findClient(int sendingClient, const char* msg, int length, int CI)
-{
-	//send to specified client
-	if(m_master.fd_array[CI] != m_socket && m_master.fd_array[CI] != sendingClient) {
-		SOCKET outSock = m_master.fd_array[CI];
-		sendToClient(outSock, msg, length);
-	}
-	
-	//for (int i = 0; i < m_master.fd_count; i++)
-	//{
-	//	SOCKET outSock = m_master.fd_array[i];
-	//	if (outSock != m_socket && outSock != sendingClient)
-	//	{
-	//		sendToClient(outSock, msg, length);
-	//	}
-	//}
-}
-
-void TCPListener::onClientConnected(int clientSocket)
+class TCPListener
 {
 
-}
+public:
 
-void TCPListener::onClientDisconnected(int clientSocket)
-{
+	TCPListener(const char* ipAddress, int port) :
+		m_ipAddress(ipAddress), m_port(port) { }
 
-}
+	// Initialize the listener
+	int init();
 
-void TCPListener::onMessageReceived(int clientSocket, const char* msg, int length, int CI)
-{
+	// Run the listener
+	int run();
 
-}
+protected:
+
+	// Handler for client connections
+	virtual void onClientConnected(int clientSocket);
+
+	// Handler for client disconnections
+	virtual void onClientDisconnected(int clientSocket);
+
+	// Handler for when a message is received from the client
+	virtual void onMessageReceived(int clientSocket, const char* msg, int length, int CI);
+
+	// Send a message to a client
+	void sendToClient(int clientSocket, const char* msg, int length);
+
+	// Broadcast a message from a client
+	void findClient(int sendingClient, const char* msg, int length, int CI);
+
+private:
+
+	const char* m_ipAddress;	// IP Address server will run on
+	int				m_port;			// Port # for the web service
+	int				m_socket;		// Internal FD for the listening socket
+	fd_set			m_master;		// Master file descriptor set
+};
+
